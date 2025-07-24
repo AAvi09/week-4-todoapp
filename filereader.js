afcbc0cee8cc39a0b0d254f6291c51ea8191616c
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-24 09:52:40 +0530

    feat: enhance file reading endpoint and add sample text file

--- filereader.js
+++ filereader.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
+
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.get("/file/:filename", (req, res) => {
-  const filename = req.params.filename;
+app.get("/files/:filename", (req, res) => {
+  const path = require("path");
+  const filename = path.join(__dirname, req.params.filename);
+  console.log("Trying to read file:", filename); // 👈 Debug
   fs.readFile(filename, "utf-8", (err, data) => {
     if (err) {
+      console.error("Error reading file:", err); // 👈 Debug
       res.status(500).send("Error reading file");
     } else {
-      res.send(data);
+      console.log("File read successfully:", data); // 👈 Debug
+      res.json({
+        data,
+      });
     }
   });
 });
