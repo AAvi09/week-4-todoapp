13789138628f5c8d98f3c8ffe16fcb39e097c032
Avinash sharma <sharmaavi2196@gmail.com>
2025-07-30 09:52:01 +0530

    feat: implement basic arithmetic operations endpoints in calculator

---
+++ calculator.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+// You have been given an express server which has a few endpoints.
+
+app.use(express.json());
+// Your task is to create a global middleware (app.use) which will
+
+app.get("/sum", (req, res) => {
+  const { a, b } = req.query;
+  const sum = parseInt(a) + parseInt(b);
+  res.status(200).json({ sum });
+});
+
+app.get("/multiply", (req, res) => {
+  const { a, b } = req.query;
+  const product = parseInt(a) * parseInt(b);
+  res.status(200).json({ product });
+});
+
+app.get("/divide", (req, res) => {
+  const { a, b } = req.query;
+  if (b == 0) {
+    return res.json({ error: "cannot divide by zero" });
+  }
+  const quotient = parseInt(a) / parseInt(b);
+  res.status(200).json({ quotient });
+});
+
+app.get("/subtract", (req, res) => {
+  const { a, b } = req.query;
+  const difference = parseInt(a) - parseInt(b);
+  res.status(200).json({ difference });
+});
+// rate limit the requests from a user to only 5 request per second
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
