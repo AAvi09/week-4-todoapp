19e71da96d744492cea7dfe503d3042896888199
Avinash sharma <sharmaavi2196@gmail.com>
2025-08-19 23:58:01 +0530

    written function to generate token

--- week-6-authentication/index.js
+++ week-6-authentication/index.js
@@ -5,11 +5,89 @@ app.use(express.json());
 
 const users = [];
 
+function tokengeneration() {
+  // This function should generate a token for the user
+  const options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  let token = "";
+  for (let i = 0; i < options.length; i++) {
+    const randomIndex = Math.floor(Math.random() * options.length);
+    token += options[randomIndex];
+  }
+  return token;
+}
+
 app.post("/signup", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
 
   users.push({ username, password });
+
+  res.json({
+    message: "User signed up successfully",
+  });
 });
 
 app.post("signin", function (req, res) {});
